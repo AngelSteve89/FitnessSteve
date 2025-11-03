14ee09b24c9ef2c9ec68ef9ae915a136f09737db
AngelSteve89 <sjangelovich@gmail.com>
2025-11-03 13:31:08 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'fitjourney-cache-v1';
+const STATIC_ASSETS = ['/', '/index.html', '/manifest.webmanifest'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      fetch(req).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put('/', clone));
+        return res;
+      }).catch(async () => {
+        const cache = await caches.open(CACHE_NAME);
+        return cache.match('/') || cache.match('/index.html');
+      })
+    );
+    return;
+  }
+  if (req.method === 'GET' && new URL(req.url).origin === location.origin) {
+    event.respondWith(
+      caches.match(req).then(cached => cached || fetch(req).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, clone));
+        return res;
+      })))
+    );
+  }
+});
