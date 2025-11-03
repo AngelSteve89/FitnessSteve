9cb2b0c02b6f2177701a9c37de6ed730dc33b07b
AngelSteve89 <sjangelovich@gmail.com>
2025-11-03 13:59:15 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import { registerServiceWorker } from './registerServiceWorker'
-
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
-
-registerServiceWorker()
