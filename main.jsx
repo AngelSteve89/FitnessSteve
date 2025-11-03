9ed189fc7eb4d31faf0e4e392c9ddf3f8c287dec
AngelSteve89 <sjangelovich@gmail.com>
2025-11-03 13:55:28 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import { registerServiceWorker } from './registerServiceWorker'
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
+
+registerServiceWorker()
